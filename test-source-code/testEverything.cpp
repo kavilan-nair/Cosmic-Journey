41d3c33a83d26545631058e3aa6b2160f38a32f0
Vorapahct <1068874@students.wits.ac.za>
2017-09-27 19:34:18 +0200

    All Tests Implemented for Game Objects

--- test-source-code/testEverything.cpp
+++ test-source-code/testEverything.cpp
@@ -3,6 +3,7 @@
 #include "Player.h"
 #include "EnemyBullet.h"
 #include "Bullet.h"
+#include "Satellites.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
@@ -198,7 +199,6 @@ TEST_CASE("Player dying at different angles causes return to origin")
 	CHECK(playerRespawnY == playerStartY);
 }
 
-
 //----------------------------Enemy Test ---------------------------------------------
 
 TEST_CASE("Enemy spawn coordinates correct")
@@ -279,7 +279,6 @@ TEST_CASE("Enemy dies when it reaches the circumference and is flagged for respa
 
 //----------------------------Bullet Test ---------------------------------------------
 
-
 TEST_CASE("Bullet spawns at player position")
 {
 	GameWindowProperties gameWindow(800,600);
@@ -337,7 +336,6 @@ TEST_CASE("Bullet status is set to alive when created")
     CHECK(isBulletAlive == aliveStatus);
 }
 
-//We should have a test for bullet hitting player but hey oh well
 TEST_CASE("Bullet status is alive after bullet update")  
 {
     GameWindowProperties gameWindow(800,600);
@@ -381,8 +379,6 @@ TEST_CASE("Bullet is fired at correct angle")
     CHECK(bullet1Angle == correctAngle1);   
 }
     
-
-
 TEST_CASE("Double bullets are fired at correct angle")
 {
     GameWindowProperties gameWindow(800,600);
@@ -427,14 +423,6 @@ TEST_CASE("Triple bullets are fired at correct angle")
 }
 
 //----------------------------Enemy Bullet Test ---------------------------------------------
-
-#include "GameWindowProperties.h"
-#include "Enemy.h"
-#include "EnemyBullet.h"
-#include "Player.h"
-
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
 
 TEST_CASE("Enemy Bullet spawns at Enemy position")
 {
@@ -493,8 +481,6 @@ TEST_CASE("Bullet status is set to dead")
     CHECK(isEnemyBulletAlive == aliveStatus);
 }
 
-//The bullet should be set to dead when it goes off the screen, implement this when the great refactoring of 2017 takes place
-
 TEST_CASE("Enemy Bullet status is alive after bullet update")  
 {
 	GameWindowProperties gameWindow(800,600);
@@ -510,8 +496,6 @@ TEST_CASE("Enemy Bullet status is alive after bullet update")
     CHECK(isEnemyBulletAlive == aliveStatus);
 }
     
-
-
 TEST_CASE("Enemy Bullet is fired at correct angle")
 {
 	GameWindowProperties gameWindow(800,600);
@@ -523,4 +507,126 @@ TEST_CASE("Enemy Bullet is fired at correct angle")
     int enemyBullet1Angle = enemyBullet.getPosition().getAngle();
     
     CHECK(enemyBullet1Angle == correctAngle);   
+}
+
+//----------------------------Satellites Test ---------------------------------------------
+
+TEST_CASE("Satellites spawn in the correct location")
+{
+	srand(time(0));
+	GameWindowProperties gameWindow(800,600);
+	int circleCenterX = gameWindow.getXOrigin();
+	int circleCenterY = gameWindow.getYOrigin();
+	int direction = rand()%360;
+	
+	for(int i = 1; i <= 3; i++)
+	{
+		Satellites spawnedSatellite(gameWindow,direction,i);
+		
+		int satelliteOriginX = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginY = spawnedSatellite.getPosition().getoriginY();
+		
+		CHECK(satelliteOriginX == circleCenterX);
+		CHECK(satelliteOriginY == circleCenterY);
+	}
+}
+
+TEST_CASE("Satellites expand first")
+{
+	GameWindowProperties gameWindow(800,600);
+	int direction = rand()%360;	
+	int toleranceBand = 2;
+	int numberSequence = 9;
+	int angleDeviation = 0;
+	float PI = atan(1)*4;
+	float factor = 0.45;
+	
+	
+	for(int j = 1; j <= 3; j++)
+	{
+		Satellites spawnedSatellite(gameWindow,direction,j);
+		for(int i = 1; i <= numberSequence; i++)
+		{
+			spawnedSatellite.move();
+		}	
+		
+		if(j == 2) angleDeviation = 20;
+		if(j == 3) angleDeviation = -20;
+		
+		auto radians = ((direction+angleDeviation) * PI/180);
+		int satelliteX = gameWindow.getXOrigin() + factor*gameWindow.getRadius()*cos(radians);
+		int satelliteY = gameWindow.getYOrigin() + factor*gameWindow.getRadius()*sin(radians);
+	
+		CHECK(spawnedSatellite.getPosition().getX() - satelliteX <= toleranceBand);
+		CHECK(spawnedSatellite.getPosition().getY() - satelliteY <= toleranceBand);
+	}
+} 
+
+TEST_CASE("Satellites change origin before gyrating")
+{
+	GameWindowProperties gameWindow(800,600);
+	int direction = rand()%360;	
+	while(direction%90 == 0)
+	{
+		direction = rand()%360;
+	}
+	
+	int numberSequence = 10;
+	
+	for(int j = 1; j <= 3; j++)
+	{
+		Satellites spawnedSatellite(gameWindow,direction,j);
+		
+		int satelliteOriginXBefore = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginYBefore = spawnedSatellite.getPosition().getoriginY();
+		
+		for(int i = 1; i <= numberSequence; i++)
+		{
+			spawnedSatellite.move();
+		}	
+		
+		int satelliteOriginXAfter = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginYAfter = spawnedSatellite.getPosition().getoriginY();
+		
+		CHECK_FALSE(satelliteOriginXBefore == satelliteOriginXAfter);
+		CHECK_FALSE(satelliteOriginYBefore == satelliteOriginYAfter);
+	}
+}
+
+TEST_CASE("Satellites gyrate correctly")
+{
+	GameWindowProperties gameWindow(800,600);
+	int direction = rand()%360;	
+	int toleranceBand = 2;
+	int beginGyrating = 10;
+	int numberSequence = rand()%10000+11;
+	float PI = atan(1)*4;
+	float gyrateRadius = 0.05*gameWindow.getRadius();
+	
+	for(int j = 1; j <= 3; j++)
+	{
+		Satellites spawnedSatellite(gameWindow,direction,j);
+		
+		for(int i = 1; i <= beginGyrating; i++)
+		{
+			spawnedSatellite.move();
+		}	
+		
+		int satelliteOriginXAfter = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginYAfter = spawnedSatellite.getPosition().getoriginY();
+		
+		for(int i = 1; i <= numberSequence; i++)
+		{
+			spawnedSatellite.move();
+		}	
+
+		float radians = (spawnedSatellite.getPosition().getAngle() * PI/180)*3;
+		auto additionX = gyrateRadius*cos(radians);
+		auto additionY = gyrateRadius*sin(radians);
+		int satelliteX = satelliteOriginXAfter + additionX;
+		int satelliteY = satelliteOriginYAfter + additionY;
+		
+		CHECK(spawnedSatellite.getPosition().getX() - satelliteX <= toleranceBand);
+		CHECK(spawnedSatellite.getPosition().getY() - satelliteY <= toleranceBand);
+	}
 }
--- test-source-code/testSatellite.cpp
+++ test-source-code/testSatellite.cpp
@@ -1,82 +1,125 @@
 #include "GameWindowProperties.h"
-#include "Enemy.h"
-
+#include "Satellites.h"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-TEST_CASE("Enemy spawn coordinates correct")
+TEST_CASE("Satellites spawn in the correct location")
 {
 	srand(time(0));
 	GameWindowProperties gameWindow(800,600);
-	Enemy spawnedEnemy(gameWindow);
-	
-	int enemyOriginX = spawnedEnemy.getPosition().getoriginX();
-	int enemyOriginY = spawnedEnemy.getPosition().getoriginY();
-	
 	int circleCenterX = gameWindow.getXOrigin();
 	int circleCenterY = gameWindow.getYOrigin();
+	int direction = rand()%360;
 	
-	CHECK(enemyOriginX == circleCenterX);
-	CHECK(enemyOriginY == circleCenterY);
+	for(int i = 1; i <= 3; i++)
+	{
+		Satellites spawnedSatellite(gameWindow,direction,i);
+		
+		int satelliteOriginX = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginY = spawnedSatellite.getPosition().getoriginY();
+		
+		CHECK(satelliteOriginX == circleCenterX);
+		CHECK(satelliteOriginY == circleCenterY);
+	}
+}
+
+TEST_CASE("Satellites expand first")
+{
+	GameWindowProperties gameWindow(800,600);
+	int direction = rand()%360;	
+	int toleranceBand = 2;
+	int numberSequence = 9;
+	int angleDeviation = 0;
+	float PI = atan(1)*4;
+	float factor = 0.45;
+	
+	
+	for(int j = 1; j <= 3; j++)
+	{
+		Satellites spawnedSatellite(gameWindow,direction,j);
+		for(int i = 1; i <= numberSequence; i++)
+		{
+			spawnedSatellite.move();
+		}	
+		
+		if(j == 2) angleDeviation = 20;
+		if(j == 3) angleDeviation = -20;
+		
+		auto radians = ((direction+angleDeviation) * PI/180);
+		int satelliteX = gameWindow.getXOrigin() + factor*gameWindow.getRadius()*cos(radians);
+		int satelliteY = gameWindow.getYOrigin() + factor*gameWindow.getRadius()*sin(radians);
+	
+		CHECK(spawnedSatellite.getPosition().getX() - satelliteX <= toleranceBand);
+		CHECK(spawnedSatellite.getPosition().getY() - satelliteY <= toleranceBand);
+	}
 } 
 
-TEST_CASE("Half movement correct location")
+TEST_CASE("Satellites change origin before gyrating")
 {
 	GameWindowProperties gameWindow(800,600);
-	Enemy spawnedEnemy(gameWindow);
-	float PI = atan(1)*4;
-	int stepSize = 99;
+	int direction = rand()%360;	
+	while(direction%90 == 0)
+	{
+		direction = rand()%360;
+	}
+	
+	int numberSequence = 10;
+	
+	for(int j = 1; j <= 3; j++)
+	{
+		Satellites spawnedSatellite(gameWindow,direction,j);
+		
+		int satelliteOriginXBefore = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginYBefore = spawnedSatellite.getPosition().getoriginY();
+		
+		for(int i = 1; i <= numberSequence; i++)
+		{
+			spawnedSatellite.move();
+		}	
+		
+		int satelliteOriginXAfter = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginYAfter = spawnedSatellite.getPosition().getoriginY();
+		
+		CHECK_FALSE(satelliteOriginXBefore == satelliteOriginXAfter);
+		CHECK_FALSE(satelliteOriginYBefore == satelliteOriginYAfter);
+	}
+}
+
+
+TEST_CASE("Satellites gyrate correctly")
+{
+	GameWindowProperties gameWindow(800,600);
+	int direction = rand()%360;	
 	int toleranceBand = 2;
-	
-	for(int i = 1; i <= stepSize; i++)
-	{
-		spawnedEnemy.move();
-	}
-	
-	auto radians = (spawnedEnemy.getPosition().getAngle() * PI/180);
-	int enemyCurrentX = gameWindow.getXOrigin() + 0.5*gameWindow.getRadius()*cos(radians);
-	int enemyCurrentY = gameWindow.getYOrigin() + 0.5*gameWindow.getRadius()*sin(radians);
-
-	CHECK(enemyCurrentX - spawnedEnemy.getPosition().getX() <= toleranceBand);
-	CHECK(enemyCurrentY - spawnedEnemy.getPosition().getY() <= toleranceBand);
-}
-
-TEST_CASE("Full movement correct location")
-{
-	GameWindowProperties gameWindow(800,600);
-	Enemy spawnedEnemy(gameWindow);
+	int beginGyrating = 10;
+	int numberSequence = rand()%10000+11;
 	float PI = atan(1)*4;
-	int stepSize = 199;
-	int toleranceBand = 2;
+	float gyrateRadius = 0.05*gameWindow.getRadius();
 	
-	for(int i = 1; i <= stepSize; i++)
+	for(int j = 1; j <= 3; j++)
 	{
-		spawnedEnemy.move();
+		Satellites spawnedSatellite(gameWindow,direction,j);
+		
+		for(int i = 1; i <= beginGyrating; i++)
+		{
+			spawnedSatellite.move();
+		}	
+		
+		int satelliteOriginXAfter = spawnedSatellite.getPosition().getoriginX();
+		int satelliteOriginYAfter = spawnedSatellite.getPosition().getoriginY();
+		
+		for(int i = 1; i <= numberSequence; i++)
+		{
+			spawnedSatellite.move();
+		}	
+
+		float radians = (spawnedSatellite.getPosition().getAngle() * PI/180)*3;
+		auto additionX = gyrateRadius*cos(radians);
+		auto additionY = gyrateRadius*sin(radians);
+		int satelliteX = satelliteOriginXAfter + additionX;
+		int satelliteY = satelliteOriginYAfter + additionY;
+		
+		CHECK(spawnedSatellite.getPosition().getX() - satelliteX <= toleranceBand);
+		CHECK(spawnedSatellite.getPosition().getY() - satelliteY <= toleranceBand);
 	}
-	
-	auto radians = (spawnedEnemy.getPosition().getAngle() * PI/180);
-	int enemyCurrentX = gameWindow.getXOrigin() + gameWindow.getRadius()*cos(radians);
-	int enemyCurrentY = gameWindow.getYOrigin() + gameWindow.getRadius()*sin(radians);
-
-	CHECK(enemyCurrentX - spawnedEnemy.getPosition().getX() <= toleranceBand);
-	CHECK(enemyCurrentY - spawnedEnemy.getPosition().getY() <= toleranceBand);
 }
-
-TEST_CASE("Enemy dies when it reaches the circumference and is flagged for respawn")
-{
-	GameWindowProperties gameWindow(800,600);
-	Enemy spawnedEnemy(gameWindow);
-	int stepSize = 300; 
-	
-	CHECK(spawnedEnemy.isAlive() == true);
-	CHECK(spawnedEnemy.isRespawn() == false);
-	
-	for(int i = 1; i <= stepSize; i++)
-	{
-		spawnedEnemy.move();
-	}
-	
-	CHECK(spawnedEnemy.isAlive() == false);
-	CHECK(spawnedEnemy.isRespawn() == true);
-}
-
