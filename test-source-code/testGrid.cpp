33975a2e39c78dc6ec807b71ad84328a6fbaa641
Kavilan Nair <kavilann@gmail.com>
2017-10-11 20:35:40 +0200

    Grid tests

---
+++ test-source-code/testGrid.cpp
@@ -0,0 +1,30 @@
+#include "Grid.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("Check grid width and height is set correctly")
+{
+    int width = 800;
+    int height = 600;
+    Grid grid{width, height};
+    
+    CHECK(grid.getWidth() == width);
+    CHECK(grid.getHeight() == height);
+}
+
+TEST_CASE("Check grid center and radius properties")
+{
+    int width = 800;
+    int height = 600;
+    Grid grid{width, height};
+    
+    int centerX = width/2;
+    int centerY = height/2;
+    int radius = (height/2)*0.95;
+    
+    CHECK(grid.getCenterX() == centerX);
+    CHECK(grid.getCenterY() == centerY);
+    CHECK(grid.getRadius() == radius);
+}
+
