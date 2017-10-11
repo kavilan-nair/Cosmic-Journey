d1a12e9020de9a35585e7f80282f36fa7b2a3714
Vorapahct <1068874@students.wits.ac.za>
2017-10-11 19:56:43 +0200

    Tests for Laser Generator & Field

---
+++ test-source-code/testLaserField.cpp
@@ -0,0 +1,80 @@
+#include "LaserField.h"
+#include "Grid.h"
+#include "EntityType.h"
+#include <memory>
+#include "vector"
+
+using std::shared_ptr;
+using std::make_shared;
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("Laser Field is initialized with correct attributes")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int location = 1;
+	shared_ptr<IMovingEntity> laserField_ptr = make_shared<LaserField>(grid,directionAngle,location);
+	
+	CHECK(laserField_ptr->getEntityType() == EntityType::LASER_FIELD);
+	CHECK(laserField_ptr->isAlive() == true);
+	CHECK(laserField_ptr->getRespawn() == false);
+	CHECK(laserField_ptr->getHitRadius() == 6);
+}
+
+TEST_CASE("Laser Field spawns at center of the grid")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int location = 1;
+	shared_ptr<IMovingEntity> laserField_ptr = make_shared<LaserField>(grid,directionAngle,location);
+	
+	CHECK(grid.getCenterX() == laserField_ptr->getPosition().getXpos());
+	CHECK(grid.getCenterY() == laserField_ptr->getPosition().getYpos());
+}
+
+TEST_CASE("Laser Field can move outwards from center")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int location = 1;
+	shared_ptr<IMovingEntity> laserField_ptr = make_shared<LaserField>(grid,directionAngle,location);
+	
+	auto xPosBefore = laserField_ptr->getPosition().getXpos();
+	auto yPosBefore = laserField_ptr->getPosition().getYpos();
+	
+	laserField_ptr->move();
+	
+	auto xPosAfter = laserField_ptr->getPosition().getXpos();
+	auto yPosAfter = laserField_ptr->getPosition().getYpos();
+	
+	bool diffPosition = (xPosAfter != xPosBefore || yPosAfter != yPosBefore);
+	
+	CHECK(diffPosition == true);
+}
+
+TEST_CASE("Laser Field can be set dead")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int location = 1;
+	shared_ptr<IMovingEntity> laserField_ptr = make_shared<LaserField>(grid,directionAngle,location);
+    
+    laserField_ptr->setDead();
+    CHECK_FALSE(laserField_ptr->isAlive());
+}
+
+TEST_CASE("Laser Field is set dead when it reaches the circumference")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int location = 1;
+	shared_ptr<IMovingEntity> laserField_ptr = make_shared<LaserField>(grid,directionAngle,location);
+        
+    while(laserField_ptr->getPosition().getRadius() < grid.getRadius()) laserField_ptr->move();
+    laserField_ptr->move();
+    
+    CHECK_FALSE(laserField_ptr->isAlive());
+    CHECK_FALSE(laserField_ptr->getRespawn());
+}
---
+++ test-source-code/testLaserGenerator.cpp
@@ -0,0 +1,79 @@
+#include "LaserGenerator.h"
+#include "Grid.h"
+#include "EntityType.h"
+#include <memory>
+#include "vector"
+
+using std::shared_ptr;
+using std::make_shared;
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("LaserGenerator is initialized with correct attributes")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int typeSide = 1;
+	shared_ptr<IMovingEntity> laserGenerator_ptr = make_shared<LaserGenerator>(grid,directionAngle,typeSide);
+	
+	CHECK(laserGenerator_ptr->getEntityType() == EntityType::LASER_GENERATOR);
+	CHECK(laserGenerator_ptr->isAlive() == true);
+	CHECK(laserGenerator_ptr->getRespawn() == false);
+	CHECK(laserGenerator_ptr->getHitRadius() == 15);
+}
+
+TEST_CASE("LaserGenerator spawns at center of the grid")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int typeSide = 1;
+	shared_ptr<IMovingEntity> laserGenerator_ptr = make_shared<LaserGenerator>(grid,directionAngle,typeSide);
+	
+	CHECK(grid.getCenterX() == laserGenerator_ptr->getPosition().getXpos());
+	CHECK(grid.getCenterY() == laserGenerator_ptr->getPosition().getYpos());
+}
+
+TEST_CASE("LaserGenerator can move")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int typeSide = 1;
+	shared_ptr<IMovingEntity> laserGenerator_ptr = make_shared<LaserGenerator>(grid,directionAngle,typeSide);
+	
+	auto xPosBefore = laserGenerator_ptr->getPosition().getXpos();
+	auto yPosBefore = laserGenerator_ptr->getPosition().getYpos();
+	
+	laserGenerator_ptr->move();
+	
+	auto xPosAfter = laserGenerator_ptr->getPosition().getXpos();
+	auto yPosAfter = laserGenerator_ptr->getPosition().getYpos();
+	
+	bool diffPosition = (xPosAfter != xPosBefore || yPosAfter != yPosBefore);
+	
+	CHECK(diffPosition == true);
+}
+
+TEST_CASE("LaserGenerator can be set dead")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int typeSide = 1;
+	shared_ptr<IMovingEntity> laserGenerator_ptr = make_shared<LaserGenerator>(grid,directionAngle,typeSide);
+    
+    laserGenerator_ptr->setDead();
+    CHECK_FALSE(laserGenerator_ptr->isAlive());
+}
+
+TEST_CASE("LaserGenerator can project laser field")
+{
+	Grid grid{800, 600};
+	int directionAngle = 20;
+	int typeSide = 1;
+	shared_ptr<IShootingMovingEntity> laserGenerator_ptr = make_shared<LaserGenerator>(grid,directionAngle,typeSide);
+	
+	auto laserFieldProjectile = laserGenerator_ptr->shoot();
+	
+	CHECK(laserFieldProjectile.size() == 1);
+	CHECK(laserFieldProjectile[0]->getEntityType() == EntityType::LASER_FIELD);
+}
