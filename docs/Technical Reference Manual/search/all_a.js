36663b95585d900811aa1a71a4360c6b93bfe15f
Kavilan Nair <kavilann@gmail.com>
2017-10-13 23:02:24 +0200

    Updated doxygen for separating data layer from presentation

--- docs/Technical Reference Manual/search/all_a.js
+++ docs/Technical Reference Manual/search/all_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['renderentities',['renderEntities',['../class_logic.html#a9a31c0b24061c7ae7a157d1482d89d69',1,'Logic']]],
   ['renderwindow',['renderWindow',['../class_presentation.html#a0f33e732141157729d261dbb9c9bd73f',1,'Presentation']]],
   ['resetmultiplier',['resetMultiplier',['../class_collision_handler.html#ad38f74096fd151c79b985bd0f0d9fcc2',1,'CollisionHandler']]],
-  ['resourcemanager',['ResourceManager',['../class_resource_manager.html',1,'ResourceManager'],['../class_resource_manager.html#a291070139c6407122e13b8711d49a4c4',1,'ResourceManager::ResourceManager()']]],
+  ['resourcemanager',['ResourceManager',['../class_resource_manager.html',1,'ResourceManager'],['../class_resource_manager.html#a3b32babd2e81909bbd90d7f2d566fadb',1,'ResourceManager::ResourceManager()'],['../class_resource_manager.html#a4355a0efcb3dd2ce06d51f6f18ce9fe6',1,'ResourceManager::ResourceManager(ResourceType resourceType, std::string resourcePath)']]],
   ['respawn',['respawn',['../class_player.html#a038a74bd768b6eec8fbe57cb2bacf811',1,'Player']]],
   ['respawnenemynormal',['respawnEnemyNormal',['../class_enemy_spawner.html#a6fda233de7a6598bd80e524405f57d14',1,'EnemySpawner']]],
   ['run',['run',['../class_logic.html#abf175ef9069615527bfbd10864b20df4',1,'Logic']]]
--- docs/Technical Reference Manual/search/functions_7.js
+++ docs/Technical Reference Manual/search/functions_7.js
@@ -3,6 +3,7 @@ var searchData=
   ['laserfield',['LaserField',['../class_laser_field.html#a2b1aa60f6a1f0dbe20b1623744c65ce8',1,'LaserField']]],
   ['laserfieldcollideswithplayer',['laserFieldCollidesWithPlayer',['../class_collision_handler.html#a4c14e009e2cadd7f53258eaa788c5357',1,'CollisionHandler']]],
   ['lasergenerator',['LaserGenerator',['../class_laser_generator.html#aedd712dfb353d61a96517b03851be992',1,'LaserGenerator']]],
-  ['loadtextures',['loadTextures',['../class_presentation.html#a6901d1af73cca32930b7c72d1a6b08cc',1,'Presentation']]],
+  ['loadtexturepaths',['loadTexturePaths',['../class_logic.html#a441fc1bc32265369c1a72b087792280f',1,'Logic']]],
+  ['loadtextures',['loadTextures',['../class_presentation.html#ad84fa951db03aa9c20d8d8ab4d429e31',1,'Presentation']]],
   ['logic',['Logic',['../class_logic.html#a65e13b69a0b4ae0c31be94bcfe11898a',1,'Logic']]]
 ];
