2bba5d406f575ab1850e66cc1e4b68c196f9cae4
Vorapahct <1068874@students.wits.ac.za>
2017-10-16 17:41:31 +0200

    testHighscoreManager set to 3000

--- test-source-code/testHighScoreManager.cpp
+++ test-source-code/testHighScoreManager.cpp
@@ -6,7 +6,7 @@
 TEST_CASE("Check HighScoreManager can retrieve score from textfile and set HighScore ")
 {
     HighScoreManager highScoreManager{};
-    int highScore = 9999;
+    int highScore = 3000;
     highScoreManager.setHighScore(highScore);
     
     CHECK(highScore == highScoreManager.getHighScore());
